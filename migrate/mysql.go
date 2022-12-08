7158ec34806e0b2d0a734aaa0bca4fec022d69ce
park-hg <jogatpark@gmail.com>
2022-12-08 23:46:53 +0900

    migrate

--- migrate/mysql.go
+++ migrate/mysql.go
@@ -5,10 +5,6 @@ import (
 	"gorm.io/gorm"
 )
 
-type mysqlRepository struct {
-	db *gorm.DB
-}
-
 func NewMysqlRepository() *gorm.DB {
 	db, err := gorm.Open(mysql.Open("root:1234@(127.0.0.1:3306)/products"), &gorm.Config{})
 	if err != nil {
